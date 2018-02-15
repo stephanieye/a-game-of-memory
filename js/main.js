7deb6bf3b6f6cf0d1f216a2501c9c2a1f1f2d555
Stephanie Ye <msstephanieye@gmail.com>
2018-02-15 22:10:14 +0000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-console.log("Up and running!");
-
 var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
+}
+};
 
-cardsInPlay.push(cardOne);
+var flipCard = function(cardID) {
 
-console.log("User flipped " + cardOne);
+console.log("User flipped " + cards[cardID]);
 
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
+cardsInPlay.push(cards[cardID]);
 
 if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+	checkForMatch();
 	}
-	else {
-		alert("Sorry, try again.");
-	}
-}
+};
+
+flipCard(0);
+flipCard(2);
+

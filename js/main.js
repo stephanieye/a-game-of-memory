e5712e995213d216c1af66ec4df4ec1467cce2d6
Stephanie Ye <msstephanieye@gmail.com>
2018-02-26 03:16:09 +0000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
 
 var cardsInPlay = [];
 
@@ -12,9 +33,12 @@ alert("Sorry, try again.");
 
 var flipCard = function(cardID) {
 
-console.log("User flipped " + cards[cardID]);
+console.log("User flipped " + cards[cardID].rank);
 
-cardsInPlay.push(cards[cardID]);
+cardsInPlay.push(cards[cardID].rank);
+
+console.log(cards[cardID].cardImage);
+console.log(cards[cardID].suit);
 
 if (cardsInPlay.length === 2) {
 	checkForMatch();

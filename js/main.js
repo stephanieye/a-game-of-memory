3c4a5e44c0d07cb8c29b0252c18728f746678c0b
Stephanie Ye <msstephanieye@gmail.com>
2018-03-02 15:59:06 +0000

    created game logic

--- js/main.js
+++ js/main.js
@@ -15,9 +15,19 @@ suit: "spades",
 cardImage: "cardimages/king_of_spades.png"
 },
 {
+rank: "rednine",
+suit: "hearts",
+cardImage: "cardimages/9_of_hearts.png"
+},
+{
 rank: "redking",
 suit: "diamonds",
 cardImage: "cardimages/king_of_diamonds.png"
+},
+{
+rank: "blackace",
+suit: "spades",
+cardImage: "cardimages/ace_of_spades.png"
 },
 {
 rank: "blackqueen",
@@ -43,11 +53,19 @@ cardImage: "cardimages/king_of_clubs.png"
 
 var cardsInPlay = [];
 
+var cardsRight = [];
+
+var cardsWrong = [];
+
 var checkForMatch = function() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-document.querySelector('.yaymsg').className = 'showmsg';
-} else {
-document.querySelector('.naymsg').className = 'showmsg';
+	if (cardsInPlay[0] === cardsInPlay[1] && cardsWrong.length === 0) {
+		cardsRight.push(cardsInPlay[0]);
+		cardsRight.push(cardsInPlay[1]);
+		document.querySelector('.yaymsg'+cardsInPlay.length).className = 'showmsg';
+} else if (cardsInPlay[0] !== cardsInPlay[1] && cardsRight.length < 8) {
+		cardsWrong.push(cardsInPlay[0]);
+		cardsWrong.push(cardsInPlay[1]);
+		document.querySelector('.naymsg').className = 'showmsg';
 }
 };
 
@@ -56,16 +74,16 @@ var flipCard = function() {
 var cardID = this.getAttribute('data-id');
 console.log("User flipped " + cards[cardID].rank);
 
-cardsInPlay.push(cards[cardID].rank);
+cardsInPlay.unshift(cards[cardID].rank);
 
 console.log(cards[cardID].cardImage);
 console.log(cards[cardID].suit);
 
-if (cardsInPlay.length <= 2) {
+if (cardsWrong.length < 2) {
 	this.setAttribute('src', cards[cardID].cardImage);
-}
+} 
 
-if (cardsInPlay.length === 2) {
+if (cardsInPlay.length % 2 === 0) {
 	checkForMatch();
 	}
 };
@@ -75,12 +93,13 @@ for (var i = 0; i < cards.length; i++) {
 	var cardElement = document.createElement('img');
 	cardElement.setAttribute('src', 'cardimages/back.png');
 	cardElement.setAttribute('data-id', i);
-	cardElement.addEventListener('click', flipCard)
+	cardElement.addEventListener('click', flipCard);
 	document.getElementById('game-board').appendChild(cardElement);
 }
 };
 
 createBoard();
+
 
 var resetButton = document.getElementById('resetButton');
 resetButton.onclick = reloadPage;
